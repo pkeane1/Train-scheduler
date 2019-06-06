73c6ea415e5f073d91c10367db5e3dfa8cad064d
Pierce Keane <Pierce.Keane@gmail.com>
2019-06-06 13:37:15 -0400

    outline of HTML done

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC-JVredgShPnKGz6lKVB873LOeQwRamag",
+    authDomain: "clickcounter2-67ecf.firebaseapp.com",
+    databaseURL: "https://clickcounter2-67ecf.firebaseio.com",
+    projectId: "clickcounter2-67ecf",
+    storageBucket: "clickcounter2-67ecf.appspot.com",
+    messagingSenderId: "376340177462",
+    appId: "1:376340177462:web:392e3c5ef15259cb"
+  };
+  // Initialize Firebase
