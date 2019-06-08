a62de4bb1b1059537c47691b65d417f2fbe762bc
Pierce Keane <Pierce.Keane@gmail.com>
2019-06-08 10:30:43 -0400

    able to push info to firebase

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,37 @@ var firebaseConfig = {
     appId: "1:376340177462:web:392e3c5ef15259cb"
   };
   // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
+// When i click submit i want to add a new train to my add train table//
+// I want to add a click event on my train forum that pushes to firebase and then to my train table//
+
+
+$("#add-train-btn").on("click",function(event){
+    event.preventDefault();
+
+    var train = $("#Train-name-input").val().trim()
+    var destination =  $("#Destination-input").val().trim()
+    var trainTime = $("#Time-input").val().trim()
+    var freq = $("#rate-input").val().trim()
+
+
+    var newTrain = {
+        name:train,
+        destination:destination,
+        time:trainTime,
+        frequency:freq
+    };
+
+    database.ref().push(newTrain)
+
+    console.log(newTrain.name);
+
+
+})
+
+
